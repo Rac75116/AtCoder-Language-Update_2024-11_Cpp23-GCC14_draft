60e2842f05ba5aa60867f9c72b331b5217bc0233
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2024-11-12 07:15:24 +0000

    modify installer: finalization

--- src/install.sh
+++ src/install.sh
@@ -6,8 +6,8 @@ sudo apt-get install -y "g++-14=${VERSION}"
 
 ### Libraries
 sudo apt-get install -y build-essential
-sudo apt-get install pigz
-sudo apt-get install pbzip2
+sudo apt-get install -y pigz
+sudo apt-get install -y pbzip2
 
 ./sub-installers/abseil.sh
 ./sub-installers/AC-Library.sh
@@ -15,3 +15,8 @@ sudo apt-get install pbzip2
 ./sub-installers/Eigen.sh
 ./sub-installers/GMP.sh
 ./sub-installers/unordered_dense.sh
+
+sudo apt-get remove -y build-essential
+sudo apt-get remove -y pigz
+sudo apt-get remove -y pbzip2
+sudo apt-get autoremove -y
